1dd20ef3201fd2b7c12cbab2b653b50235ce46db
mussaratshamsher <musaratskhan@github.com>
2025-03-08 20:16:45 +0500

    update

--- growth-mindset/app.py
+++ growth-mindset/app.py
@@ -4,7 +4,7 @@ import os
 from io import BytesIO  
 
 # App setup  
-st.set_page_config(page_title="Growth Mindset Challenge",page_icon="🎨🌱" layout='wide')  
+st.set_page_config(page_title="Growth Mindset Challenge",page_icon="🎨🌱",  layout='wide')  
 
 # Sidebar setup  
 st.sidebar.title("Mussarat Shamsher")  
