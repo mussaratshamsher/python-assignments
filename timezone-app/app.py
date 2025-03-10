85262e5a3d716bf8ec5b4232f4d01602b24dd05c
mussaratshamsher <musaratskhan@github.com>
2025-03-10 11:33:35 +0500

    update

--- timezone-app/app.py
+++ timezone-app/app.py
@@ -114,7 +114,6 @@ st.markdown(
     transform: scale(1);
     animation: animateBorder 2s linear infinite;
 }
-
     </style>
     """, 
     unsafe_allow_html=True
@@ -158,6 +157,7 @@ if st.button("Convert Time"):
     dt = datetime.combine(datetime.today(), current_time, tzinfo=ZoneInfo(from_tz))
     converted_time = dt.astimezone(ZoneInfo(to_tz)).strftime("%Y-%m-%d %I:%M:%S %p")
     st.success(f"Converted Time in {to_tz}: {converted_time}")
-    st.balloons()
+    st.snow()
+    
 
     
